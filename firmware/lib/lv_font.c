56e811bfae2975f1e1c2d09071eb497428577a24
Michael Betz <michibetz@gmail.com>
2020-08-03 11:36:03 -0700

    add LVLG license header

--- firmware/lib/lv_font.c
+++ firmware/lib/lv_font.c
@@ -1,8 +1,28 @@
+// MIT licence
+// Copyright (c) 2020 LVGL LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 // This is the essence of the LVGL bitmap-font engine.
-// Code is mostly verbatim copied from LVGL and stripped down
+// Code is mostly verbatim copied from LVGL and stripped down:
 //   * no compression (not effective for small sizes anyway)
 //   * only 4 bpp
-// TODO check how to deal with the license
 
 #include <stdio.h>
 #include "lv_font.h"
